b2609e96bfe32dd08b1e52486e6f8545f7ef3690
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-12-22 13:15:08 +0100

    scaffold header file for main
    main header will handle game state transitions

---
+++ vectorogue/src/include/main.h
@@ -0,0 +1,10 @@
+//
+// Created by nanotome on 12/22/2024.
+//
+
+#ifndef VECTOROGUE_MAIN_H
+#define VECTOROGUE_MAIN_H
+
+void LoadComplete();
+
+#endif //VECTOROGUE_MAIN_H
--- vectorogue/src/main.c
+++ vectorogue/src/main.c
@@ -1,24 +1,21 @@
-/*******************************************************************************************
-*
-*   raylib [core] examples - basic screen manager
-*
-*   NOTE: This example illustrates a very simple screen manager based on a states machines
-*
-*   Example originally created with raylib 4.0, last time updated with raylib 4.0
-*
-*   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
-*   BSD-like license that allows static linking with closed source software
-*
-*   Copyright (c) 2021-2024 Ramon Santamaria (@raysan5)
-*
-********************************************************************************************/
-
 #include "raylib.h"
+#include "main.h"
 
 //------------------------------------------------------------------------------------------
 // Types and Structures Definition
 //------------------------------------------------------------------------------------------
-typedef enum GameScreen { LOGO = 0, TITLE, GAMEPLAY, ENDING } GameScreen;
+typedef enum ApplicationStates {
+    STARTUP = 0,
+    LOADING,
+    MENU,
+    RUNNING,
+    PAUSED,
+    GAMEOVER,
+    QUITTING
+} ApplicationState;
+
+ApplicationState currentState = STARTUP;
+int framesCounter = 0;
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -27,16 +24,12 @@ int main(void)
 {
     // Initialization
     //--------------------------------------------------------------------------------------
-    const int screenWidth = 800;
-    const int screenHeight = 450;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
 
-    InitWindow(screenWidth, screenHeight, "vectorogue");
-
-    GameScreen currentScreen = LOGO;
+    InitWindow(screenWidth, screenHeight, "Vectorogue");
 
     // TODO: Initialize all required variables and load all required data here!
-
-    int framesCounter = 0;          // Useful to count frames
 
     SetTargetFPS(60);               // Set desired framerate (frames-per-second)
     //--------------------------------------------------------------------------------------
@@ -46,48 +39,40 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        switch(currentScreen)
+        switch(currentState)
         {
-            case LOGO:
+            case STARTUP:
             {
-                // TODO: Update LOGO screen variables here!
-
-                framesCounter++;    // Count frames
-
-                // Wait for 2 seconds (120 frames) before jumping to TITLE screen
-                if (framesCounter > 120)
-                {
-                    currentScreen = TITLE;
-                }
+                LoadComplete();
             } break;
-            case TITLE:
+            case MENU:
             {
                 // TODO: Update TITLE screen variables here!
 
                 // Press enter to change to GAMEPLAY screen
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
-                    currentScreen = GAMEPLAY;
+                    currentState = RUNNING;
                 }
             } break;
-            case GAMEPLAY:
+            case RUNNING:
             {
                 // TODO: Update GAMEPLAY screen variables here!
 
                 // Press enter to change to ENDING screen
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
-                    currentScreen = ENDING;
+                    currentState = QUITTING;
                 }
             } break;
-            case ENDING:
+            case QUITTING:
             {
                 // TODO: Update ENDING screen variables here!
 
                 // Press enter to return to TITLE screen
                 if (IsKeyPressed(KEY_ENTER) || IsGestureDetected(GESTURE_TAP))
                 {
-                    currentScreen = TITLE;
+                    currentState = MENU;
                 }
             } break;
             default: break;
@@ -100,16 +85,16 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            switch(currentScreen)
+            switch(currentState)
             {
-                case LOGO:
+                case STARTUP:
                 {
                     // TODO: Draw LOGO screen here!
                     DrawText("LOGO SCREEN", 20, 20, 40, LIGHTGRAY);
-                    DrawText("WAIT for 2 SECONDS...", 290, 220, 20, GRAY);
+                    DrawText("WAIT for 1 SECOND...", 290, 220, 20, GRAY);
 
                 } break;
-                case TITLE:
+                case MENU:
                 {
                     // TODO: Draw TITLE screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
@@ -117,7 +102,7 @@ int main(void)
                     DrawText("PRESS ENTER or TAP to JUMP to GAMEPLAY SCREEN", 120, 220, 20, DARKGREEN);
 
                 } break;
-                case GAMEPLAY:
+                case RUNNING:
                 {
                     // TODO: Draw GAMEPLAY screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
@@ -125,7 +110,7 @@ int main(void)
                     DrawText("PRESS ENTER or TAP to JUMP to ENDING SCREEN", 130, 220, 20, MAROON);
 
                 } break;
-                case ENDING:
+                case QUITTING:
                 {
                     // TODO: Draw ENDING screen here!
                     DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
@@ -149,4 +134,16 @@ int main(void)
     //--------------------------------------------------------------------------------------
 
     return 0;
+}
+
+void LoadComplete() {
+    // TODO: Update LOGO screen variables here!
+
+    framesCounter++;    // Count frames
+
+    // Wait for 2 seconds (120 frames) before jumping to TITLE screen
+    if (framesCounter > 60)
+    {
+        currentState = MENU;
+    }
 }
