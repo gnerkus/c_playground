6e5711601dae23d7fedaedb4b41eefa05bf7ffd5
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-12-25 15:08:53 +0100

    handle non arrow actor types
    handle up arrow actor

--- vectorogue/src/game.c
+++ vectorogue/src/game.c
@@ -38,6 +38,7 @@ static ActorTypes actors[TILE_COUNT];
  * useful for determining which type of actor can be added to the board
  */
 static int actorCounts[ACTOR_TYPE_COUNT];
+static int tempEmpty[TILE_COUNT];
 
 static int timer = 0;
 
@@ -99,6 +100,13 @@ void DebugBoard() {
     printf("BOARD PRINTED: END");
 }
 
+void DebugPlayerState() {
+    printf("PLAYER STATE: START\n");
+    printf("HP: %d\n", playerHP);
+    printf("coins: %d\n", playerCoins);
+    printf("PLAYER STATE: END");
+}
+
 // ----------------------------------------------------------
 // PUBLIC (in header file)
 // ----------------------------------------------------------
@@ -114,6 +122,11 @@ void InitGame() {
 
     // grid is full of empty tiles at start
     actorCounts[EMPTY] = TILE_COUNT;
+
+    // keep track of empty tiles when handling arrow actors
+    for (int i = 0; i < TILE_COUNT; ++i) {
+        tempEmpty[i] = -1;
+    }
 
     // add indices of empty tile slots to list of empty tiles
     for (int i = 0; i < TILE_COUNT; ++i) {
@@ -134,12 +147,81 @@ void InitGame() {
     currentState = RUNNING;
 }
 
+int GetIdxFromRowCol(int row, int col) {
+    return row * GRID_WIDTH + col;
+}
+
+int GetRowFromIdx(int idx) {
+    return idx / GRID_WIDTH;
+}
+
+int GetColFromIdx(int idx) {
+    return idx % GRID_WIDTH;
+}
+
+void HandleUpArrow() {
+    for (int i = 0; i < TILE_COUNT; ++i) {
+        tempEmpty[i] = i;
+        ActorTypes currentActor = actors[i];
+        int row = GetRowFromIdx(i);
+        int col = GetColFromIdx(i);
+        if (row > 0) {
+            int new_pos = GetIdxFromRowCol(row - 1, col);
+            actors[new_pos] = currentActor;
+            // current position is now occupied
+            tempEmpty[new_pos] = -1;
+        }
+        actors[i] = EMPTY;
+    }
+}
+
+void HandleTurn() {
+    for (int i = 0; i < TILE_COUNT; ++i) {
+        ActorTypes currentActor = actors[i];
+        switch (currentActor) {
+            case UP_ARROW:
+                HandleUpArrow();
+                break;
+            case ACTIVE_MONSTER:
+                playerHP -= 1;
+                break;
+            case ACTIVE_COINS:
+                playerCoins += 1;
+                break;
+            case ACTIVE_POTION:
+                playerHP += 1;
+                break;
+            case INERT_POTION:
+            case INERT_MONSTER:
+            case INERT_COINS:
+            case EMPTY:
+            case LAST:
+            default:
+                break;
+        }
+        if (playerHP <= 0) {
+            currentState = GAMEOVER;
+            break;
+        }
+    }
+
+    // update empty tiles
+    for (int i = 0; i < TILE_COUNT; ++i) {
+        if (tempEmpty[i] > -1) {
+            pushToEmpty(tempEmpty[i]);
+            tempEmpty[i] = -1;
+        }
+    }
+
+    shuffleEmpty();
+}
+
 void UpdateGame() {
     if (currentState != GAMEOVER) {
         if (currentState != PAUSED) {
             // timer; runs every 1/turnSpeed seconds
             if ((timer % (60 / turnSpeed)) == 0) {
-                printf("TURN");
+                HandleTurn();
             }
 
             framesCounter++;
