387522c79df4815da8a003062232496e4077e079
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2024-12-31 17:47:19 +0100

    scaffold ui for gameplay

---
+++ vectorogue/src/gameplay_ui.c
@@ -0,0 +1,15 @@
+//
+// Created by nanotome on 12/31/2024.
+//
+
+void DrawPlayerHP() {
+
+}
+
+void DrawTimer() {
+
+}
+
+void DrawPlayerCoins() {
+
+}
---
+++ vectorogue/src/include/gameplay_ui.h
@@ -0,0 +1,12 @@
+//
+// Created by nanotome on 12/31/2024.
+//
+
+#ifndef VECTOROGUE_GAMEPLAY_UI_H
+#define VECTOROGUE_GAMEPLAY_UI_H
+
+void DrawPlayerHP();
+void DrawTurnTimer();
+void DrawPlayerCoins();
+
+#endif //VECTOROGUE_GAMEPLAY_UI_H
